d32e0292d4e3c73861b93c8d08d43cd8fdf14e33
Louis Labeyrie <labeyrielouis@gmail.com>
2024-08-26 14:01:41 +0200

    chore: Refactor observer.py
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- observer.py
+++ observer.py
@@ -5,6 +5,7 @@ import signal
 from subprocess import PIPE, Popen
 import time
 
+
 async def main():
     while True:
         try:
@@ -15,30 +16,34 @@ async def main():
             for file_name in file_list:
                 file = open("world/stats/" + file_name, "r")
                 json_data = json.load(file)
-                if json_data['stats']['minecraft:deaths'] > 0:
+                if json_data["stats"]["minecraft:deaths"] > 0:
                     kill_process()
                     start_server()
-        except:
-            print("Error reading file")
+        except Exception as e:
+            print("Error reading file:", str(e))
 
         await asyncio.sleep(5)
 
+
 def kill_process():
-    processes = Popen(['ps', '-ef'], stdout=PIPE, stderr=PIPE)
+    processes = Popen(["ps", "-ef"], stdout=PIPE, stderr=PIPE)
     stdout, _ = processes.communicate()
 
     for line in stdout.splitlines():
 
-        if (line.__contains__("java")):
+        if line.__contains__("java"):
 
             pid = int(line.split(None, 1)[0])
             os.kill(pid, signal.SIGKILL)
 
+
 def start_server():
     os.system("java -Xmx1024M -Xms1024M -jar server.jar nogui")
 
+
 def rename_world():
     os.system("mv world world_" + str(time.time()))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
