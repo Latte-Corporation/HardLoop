fd672760290677a0dca81bf9dd2a525d3a18261e
Dorian TETU <doriantetu34@gmail.com>
2024-08-27 13:51:07 +0200

    chore: Update pyproject.toml to include aioping dependency and server.properties to disable broadcasting RCON to ops

--- hardloop/hardloop/observer.py
+++ hardloop/hardloop/observer.py
@@ -1,6 +1,7 @@
 import asyncio
 from mcrcon import MCRcon
 from .server import update_motd
+import aioping
 
 last_dead_player = "Unknown"
 
@@ -8,23 +9,21 @@ last_dead_player = "Unknown"
 async def listen():
     global last_dead_player
     print("Listening for deaths")
-    while True:
-        try:
-            with MCRcon("0.0.0.0", "password", port=25575) as client:
+    with MCRcon("0.0.0.0", "password", port=25575) as client:
+        while True:
+            try:
                 response = client.command("/scoreboard players list")
                 if "There are no tracked entities" not in response:
                     last_dead_player = response.split(": ")[1]
                     client.command(f"/kick @a {last_dead_player} is dead !")
                     update_motd(last_dead_player)
-                    await reset_server()
-        except Exception:
-            asyncio.sleep(0)
-            print("error fetching deaths")
+                    await reset_server(client)
+                    break
+            except Exception:
+                print("error fetching deaths")
 
-        await asyncio.sleep(0.1)
+            await asyncio.sleep(0.1)
 
 
-async def reset_server():
-    with MCRcon("0.0.0.0", "password", port=25575) as client:
-        client.command("/stop")
-        print("Sending stop command")
+async def reset_server(client):
+    client.command("/stop")
