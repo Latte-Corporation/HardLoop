832591186f7de34d574124731a46e2b1f0ecbe6e
Louis Labeyrie <labeyrielouis@gmail.com>
2024-08-27 13:48:48 +0200

    chore: Update docker-compose.yml and main app to include environment variables for Minecraft version and port
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- hardloop/hardloop/main.py
+++ hardloop/hardloop/main.py
@@ -3,12 +3,16 @@ from .observer import listen
 from .server import start_server, rename_world
 
 import asyncio
+import os
+
+mc_version = os.getenv("MC_VERSION", None)
+mc_port = os.getenv("MC_PORT", 25565)
 
 
 async def main():
-    download_server_file()
+    download_server_file(mc_version)
     while True:
-        server_process = await start_server()
+        server_process = await start_server(mc_port)
         listen_task = asyncio.create_task(listen())
 
         await asyncio.to_thread(server_process.wait)
